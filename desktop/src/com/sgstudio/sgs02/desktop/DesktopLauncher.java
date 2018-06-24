f8867748e4082fbd5168a0342e5ed9ef7c0305dd
Unknown <yarikbur@Mail.ru>
2018-06-24 13:57:54 +0300

    Init LibGDX

---
+++ desktop/src/com/sgstudio/sgs02/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sgstudio.sgs02.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sgstudio.sgs02.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
